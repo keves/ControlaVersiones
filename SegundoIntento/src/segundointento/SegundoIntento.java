131bb7fd5f2efa8da322165e6472a58aef773f9b
CP13 <CP13@172.21.104.11>
2015-10-20 11:37:05 -0500

    by: PcUniversidad

---
+++ SegundoIntento/src/segundointento/SegundoIntento.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundointento;
+
+/**
+ *
+ * @author CP13
+ */
+public class SegundoIntento {
+
+   public void sumas ()
+   {
+       int a=0,b=0;
+       int c = a+b;
+   }
+    public static void main(String[] args) 
+    {
+       
+        
+    }
+    
+}
